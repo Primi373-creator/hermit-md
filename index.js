cce20bca48d0532db80f3318c56d5cb5b61d6b1f
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2023-10-13 18:30:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const client = require('./lib/client')
 
 const connect = async () => {
-	try {
-		await client.connect()
-	} catch (error) {
-		console.error(error)
-	}
+ try {
+  await client.connect()
+ } catch (error) {
+  console.error(error)
+ }
 }
+
+const app = express()
+app.listen(8080, () => {
+ console.log('Server listening on port 8080')
+})
 
 connect()
